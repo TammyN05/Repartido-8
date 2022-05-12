84760525e09898460ad393fe56ad1eb09518f67a
braian9 <braianmartinez123456@gmail.com>
2022-05-12 17:59:17 -0300

    Tam

---
+++ script.js
@@ -0,0 +1,51 @@
+/* EJERCICIO 1: Al pasar por encima de cada uno de los párrafos
+deberán cambiar el color del texto, 
+el tamaño y el borde. (mouseover y mouseout). */
+
+
+
+var p = document.querySelectorAll("p")
+.addEventListener("mouseout", uno);
+
+
+function uno() {
+    for (let i = 0; i <list.length; i++) {
+        list[i].style.textColor="red"
+    }
+    document.querySelectorAll("p").innerHTML;
+  }
+
+
+
+
+
+/* EJERCICIO 2: En el formulario de contacto se deberá “guardar” el nombre 
+e imprimir en un div vacío el contenido “Gracias [nombre ingresado] por comunicarse con nosotros”. 
+Los datos se guardarán con el botón. 
+ */
+
+
+
+
+
+
+
+
+
+/* EJERCICIO 3: Los 4 botones serán utilizados para cambiar el color del background de la página 
+(se puede también usar background imagen. (click o onclick)
+ */
+
+
+
+
+
+
+
+
+
+
+
+/* EJECICIO 4: Dentro del primer div vacío deberás crear un elemento imagen (createElement)
+ y asignarlo a ese div (appendChild). Todo eso se hará una vez se clickee el botón restante.
+ */
